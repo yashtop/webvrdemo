a4fe9d390e869e0b17643d9eeed9a382d35f212a
suyash mishra <suyash.a.mishra@accenture.com>
2018-04-04 19:27:18 +0530

    single screen implementation

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
-<?php
-$page = $_REQUEST['page'];
-switch($page){
-  1:
-  include_once("index.html");
-  break;
-  2:
-  include_once("index_single.html");
-  break;
-  default:
-  include_once("index.html");
-}
- ?>
+<?php include_once("index.html"); ?>
